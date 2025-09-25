047f42bcb33103cbec516e7f3d14aec143221f93
wilfried-wayou <wilfried.wayou@2026.ucac-icam.com>
2025-09-25 18:18:42 +0100

    Add PrimeIcons, Header component, and integrate Badge modules

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Button} from 'primeng/button';
-
+import {IconField} from 'primeng/iconfield';
+import {Badge} from 'primeng/badge';
+import { BadgeModule } from 'primeng/badge';
+import {OverlayBadge, OverlayBadgeModule} from 'primeng/overlaybadge';
+import {Header} from './header/header';
 @Component({
   selector: 'app-root',
-  imports: [Button],
+  imports: [Button, IconField, Badge, OverlayBadge, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
